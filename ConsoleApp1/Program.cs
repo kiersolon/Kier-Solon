309046616ff17c772c8b831336d58f3746e5bc3d
unknown <Admin@CSLAB243.CSSERVER.local>
2024-02-01 19:42:42 +0800

    arithmetic

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Activity1
+{
+    public class Math
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+                  
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Math math = new Math();
+
+                int num1, num2;
+
+                Console.WriteLine("Enter a first Number: ");
+                num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter a second Number: ");
+                num2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Addition: " + (num1 + num2));
+                Console.WriteLine("Substraction: " + (num1 - num2));
+                Console.WriteLine("Multiplication: " + (num1 * num2));
+                Console.WriteLine("Division: " + (num1 / num2));
+
+                Console.Beep();
+                Console.ReadKey();
+            }
+
+
+
+        }
+    }
+}
